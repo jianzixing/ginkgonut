9a0b9d06ffaadb5971f108a62aa4858c2208ce57
yangankang <yak1992@foxmail.com>
2020-05-08 20:37:37 +0800

    提交

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import Ginkgo, {CSSProperties} from "ginkgoes";
+import "./index.scss";
+import App from "./skeleton/App";
+import GinkgoT2View from "./GinkgoT2View";
+
+
+Ginkgo.render(<App/>, document.body);
+// Ginkgo.render(<GinkgoT2View/>, document.body);
