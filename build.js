fcedf3d254355d3bfd9f179f4c0c46141fbfe620
yangankang <yak1992@foxmail.com>
2020-05-21 23:06:22 +0800

    提交

--- build.js
+++ build.js
@@ -3,13 +3,6 @@ const sass = require('node-sass');
 const fs = require('fs');
 const path = require('path');
 
-async function runBuildWebpack() {
-    const child1 = await execa('./node_modules/webpack/bin/webpack.js', ["--config", "webpack.config.js"], {});
-    const child2 = await execa('./node_modules/typescript/bin/tsc');
-
-}
-
-
 function readFileList(dir, filesList = []) {
     const files = fs.readdirSync(dir);
     files.forEach((item, index) => {
@@ -24,54 +17,64 @@ function readFileList(dir, filesList = []) {
     return filesList;
 }
 
+async function runBuildWebpack() {
+    console.log("start webpack...");
+    const child1 = await execa('./node_modules/webpack/bin/webpack.js', ["--config", "webpack.config.js"], {});
+    console.log("start tsc...");
+    const child2 = await execa('./node_modules/typescript/bin/tsc');
 
-let filesList = [];
-readFileList("./src", filesList);
+    console.log("start build scss...");
+    let filesList = [];
+    readFileList("./src", filesList);
 
-let scssImports = [];
-let scssExports = [];
+    let scssImports = [];
+    let scssExports = [];
 
-for (let file of filesList) {
-    let path = file.toString();
-    if (path.endsWith(".scss") &&
-        !(path.startsWith("src/icon") && path.indexOf("_") >= 0)) {
-        scssImports.push("@import \"" + path + "\";");
-        let outPath = path.substring(3, path.length - 5);
-        scssExports.push("@import \"./lib" + path.substring(3) + "\";");
-        let outFile = __dirname + "/" + "dist/lib" + outPath + ".css";
-        let result = sass.renderSync({
-            file: __dirname + "/" + path,
-            outFile: __dirname + "/" + outFile,
-            sourceMap: true
-        })
-        if (result) {
-            fs.writeFileSync(outFile,
-                result.css,
-                function (err) {
-                    if (!err) {
-                        //file written on disk
+    for (let file of filesList) {
+        let path = file.toString();
+        if (path.endsWith(".scss") &&
+            !(path.startsWith("src/icon") && path.indexOf("_") >= 0)) {
+            scssImports.push("@import \"" + path + "\";");
+            let outPath = path.substring(3, path.length - 5);
+            scssExports.push("@import \"./lib" + path.substring(3) + "\";");
+            let outFile = __dirname + "/" + "dist/lib" + outPath + ".css";
+            let result = sass.renderSync({
+                file: __dirname + "/" + path,
+                outFile: __dirname + "/" + outFile,
+                sourceMap: true
+            })
+            if (result) {
+                fs.writeFileSync(outFile,
+                    result.css,
+                    function (err) {
+                        if (!err) {
+                            //file written on disk
+                        }
+                    });
+                fs.writeFileSync(__dirname + "/" + "dist/lib" + path.substring(3),
+                    fs.readFileSync(__dirname + "/" + path),
+                    function (err) {
+
                     }
-                });
-            fs.writeFileSync(__dirname + "/" + "dist/lib" + path.substring(3),
-                fs.readFileSync(__dirname + "/" + path),
-                function (err) {
-
-                }
-            )
+                )
+            }
         }
     }
-}
 
-let result = sass.renderSync({
-    data: scssImports.join("\n"),
-});
-fs.writeFileSync(__dirname + "/" + "dist/ginkgonut.scss", scssExports.join("\n"), function (err) {
-
-})
-fs.writeFileSync(__dirname + "/" + "dist/ginkgonut.css", result.css, function (err) {
-
-})
-fs.writeFileSync(__dirname + "/" + "dist/package.json",
-    fs.readFileSync(__dirname + "/package.dist.json"), function (err) {
+    let result = sass.renderSync({
+        data: scssImports.join("\n"),
+    });
+    fs.writeFileSync(__dirname + "/" + "dist/ginkgonut.scss", scssExports.join("\n"), function (err) {
 
     })
+    // fs.writeFileSync(__dirname + "/" + "dist/ginkgonut.css", result.css, function (err) {
+    //
+    // })
+    fs.writeFileSync(__dirname + "/" + "dist/package.json",
+        fs.readFileSync(__dirname + "/package.dist.json"), function (err) {
+
+        })
+    console.log("finish")
+}
+
+runBuildWebpack();
